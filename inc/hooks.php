1585172fc010451a39c1c967e7ee8c96d5caf2b2
ruslan <rus@ruslan.mx>
2019-09-06 16:26:44 -0500

    150px logo width

--- inc/hooks.php
+++ inc/hooks.php
@@ -27,24 +27,11 @@ if ( ! function_exists( 'understrap_add_site_info' ) ) {
 		$the_theme = wp_get_theme();
 
 		$site_info = sprintf(
-			'<a href="%1$s">%2$s</a><span class="sep"> | </span>%3$s(%4$s)',
-			esc_url( __( 'http://wordpress.org/', 'understrap' ) ),
-			sprintf(
-				/* translators:*/
-				esc_html__( 'Proudly powered by %s', 'understrap' ),
-				'WordPress'
-			),
+			'© Natural Option. All Rights Reserved.',
+			esc_url( __( 'https://naturaloption.com', 'natural-option' ) ),
 			sprintf( // WPCS: XSS ok.
 				/* translators:*/
-				esc_html__( 'Theme: %1$s by %2$s.', 'understrap' ),
-				$the_theme->get( 'Name' ),
-				'<a href="' . esc_url( __( 'http://understrap.com', 'understrap' ) ) . '">understrap.com</a>'
-			),
-			sprintf( // WPCS: XSS ok.
-				/* translators:*/
-				esc_html__( 'Version: %1$s', 'understrap' ),
-				$the_theme->get( 'Version' )
-			)
+				esc_html__( '© Natural Option. All Rights Reserved.', 'natural' )			)
 		);
 
 		echo apply_filters( 'understrap_site_info_content', $site_info ); // WPCS: XSS ok.
