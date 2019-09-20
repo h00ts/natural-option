2801af106b39a05e701074f9fadf5f7ca77f8d9b
ruslan <rus@ruslan.mx>
2019-09-20 00:50:03 -0500

    remove crumbs

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -31,6 +31,8 @@ if ( ! function_exists( 'understrap_woocommerce_support' ) ) {
  */
 remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10 );
 remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10 );
+remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20, 0 );
+
 
 /**
  * Then hook in your own functions to display the wrappers your theme requires
