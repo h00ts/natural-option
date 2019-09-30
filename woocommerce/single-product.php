7be09e2ebab15f01ce15389471144a1003c9253c
ruslan <rus@ruslan.mx>
2019-09-30 13:40:13 -0500

    rearrange product descritions

--- woocommerce/single-product.php
+++ woocommerce/single-product.php
@@ -31,11 +31,11 @@ get_header( 'shop' ); ?>
 		do_action( 'woocommerce_before_main_content' );
 	?>
 
-		<?php while ( have_posts() ) : the_post(); ?>
+		<?php /***while ( have_posts() ) : the_post(); ?>
 
 			<?php wc_get_template_part( 'content', 'single-product' ); ?>
 
-		<?php endwhile; // end of the loop. ?>
+		<?php endwhile; // end of the loop. ***/?>
 
 	<?php
 		/**
