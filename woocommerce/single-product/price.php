5f474bde7dbfe29944e5fa951f69380b06f4a2a8
ruslan <rus@ruslan.mx>
2019-10-01 12:08:02 -0500

    crazy hover effect lol

--- woocommerce/single-product/price.php
+++ woocommerce/single-product/price.php
@@ -13,7 +13,7 @@
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce/Templates
  * @version 3.0.0
- *
+ */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
@@ -23,5 +23,3 @@ global $product;
 
 ?>
 <p class="<?php echo esc_attr( apply_filters( 'woocommerce_product_price_class', 'price' ) );?>"><?php echo $product->get_price_html(); ?></p>
-
-**/
